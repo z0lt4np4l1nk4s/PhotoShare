371b74fea590fefe36932cfad7b30a26b21aacc5
z0lt4np4l1nk4s <65424998+z0lt4np4l1nk4s@users.noreply.github.com>
2020-08-10 16:35:32 +0200

    Add files via upload

--- Controllers/PhotoVideoController.cs
+++ Controllers/PhotoVideoController.cs
@@ -9,30 +9,50 @@ using System.Web.Mvc;
 
 namespace PhotoShare.Controllers
 {
+    [Authorize]
     public class PhotoVideoController : Controller
     {
         List<string> ImageExtensions = new List<string> { ".jpg", ".jpe", ".bmp", ".gif", ".png", ".jpeg", ".jfif" };
         PhotoVideoDBContext db = new PhotoVideoDBContext();
+        bool isEdit = false;
         // GET: PhotoVideo
+        
         public ActionResult Create()
+        {
+            isEdit = false;
+            FillListBox(0);
+            return View();
+        }
+
+        private void FillListBox(int id)
         {
             if (db.Tag.Count() != 0)
             {
                 List<SelectListItem> list = new List<SelectListItem>();
-                foreach(Tag t in db.Tag)
+                foreach (Tag t in db.Tag)
                 {
                     SelectListItem selectListItem = new SelectListItem();
                     selectListItem.Value = t.ID.ToString();
                     selectListItem.Text = t.Naziv;
+                    if (isEdit)
+                    {
+                        foreach (PhotoVideoTag pvt in db.PhotoVideoTag)
+                        {
+                            if (pvt.PhotoVideoID == id && pvt.TagID == t.ID)
+                            {
+                                selectListItem.Selected = true;
+                                continue;
+                            }
+                        }
+                    }
                     list.Add(selectListItem);
                 }
                 Session["Tag"] = list;
             }
             else
             {
-                Session["Tag"] = null; 
+                Session["Tag"] = null;
             }
-            return View();
         }
 
         [HttpPost]
@@ -44,24 +64,7 @@ namespace PhotoShare.Controllers
             }
             else
             {
-                List<string> text = new List<string>();
-                StringBuilder sb = new StringBuilder();
-                foreach (char c in tag)
-                {
-                    if (!char.IsDigit(c) && !char.IsLetter(c))
-                    {
-                        if (sb.ToString() != "")
-                        {
-                            text.Add(sb.ToString());
-                        }
-                        sb.Clear();
-                    }
-                    else
-                    {
-                        sb.Append(c);
-                    }
-                }
-                text.Add(sb.ToString());
+                List<string> text = GetTagsFromText(tag);
                 photoVideo.Naziv = Path.GetFileNameWithoutExtension(photoVideo.PhotoVideoFile.FileName);
                 if (ImageExtensions.Contains(Path.GetExtension(photoVideo.PhotoVideoFile.FileName)))
                 {
@@ -90,6 +93,29 @@ namespace PhotoShare.Controllers
                 }
                 
             }
+        }
+
+        private List<string> GetTagsFromText(string tag)
+        {
+            List<string> text = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in tag)
+            {
+                if (!char.IsDigit(c) && !char.IsLetter(c))
+                {
+                    if (sb.ToString() != "")
+                    {
+                        text.Add(sb.ToString());
+                    }
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            text.Add(sb.ToString());
+            return text;
         }
 
         private void AddTagToPost(int id, string[] tags, List<string> list)
@@ -122,6 +148,79 @@ namespace PhotoShare.Controllers
                 }
             }
             db.SaveChanges();
+        }
+
+        private void RemoveTagsFromPost(int id)
+        {
+            db.PhotoVideoTag.RemoveRange(db.PhotoVideoTag.Where(x => x.PhotoVideoID == id));
+        }
+
+        public ActionResult Details(int id)
+        {
+            ViewBag.PostTags = db.PhotoVideoTag;
+            ViewBag.Tag = db.Tag;
+            return View(db.PhotoVideo.Single(x => x.ID == id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.PostTags = db.PhotoVideoTag;
+            ViewBag.Tag = db.Tag;
+            isEdit = true;
+            FillListBox(id);
+            return View(db.PhotoVideo.Single(x => x.ID == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PhotoVideo photoVideo, string[] tags, string tag)
+        {
+            PhotoVideo pv = db.PhotoVideo.Single(x => x.ID == photoVideo.ID);
+            if (tags == null && string.IsNullOrEmpty(tag))
+            {
+                return View(db.PhotoVideo.Single(x => x.ID == photoVideo.ID));
+            }
+            else
+            {
+                RemoveTagsFromPost(photoVideo.ID);
+                AddTagToPost(photoVideo.ID, tags, GetTagsFromText(tag));
+                if (pv.isSlika)
+                {
+                    return RedirectToAction("Slika", "Home");
+                }
+                else
+                {
+                    return RedirectToAction("Video", "Home");
+                }
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ViewBag.PostTags = db.PhotoVideoTag;
+            ViewBag.Tag = db.Tag;
+            return View(db.PhotoVideo.Single(x => x.ID == id));
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PhotoVideo photoVideo = db.PhotoVideo.Single(x => x.ID == id);
+            if (System.IO.File.Exists(Request.MapPath(photoVideo.Path)))
+            {
+                System.IO.File.Delete(Request.MapPath(photoVideo.Path));
+            }
+            db.PhotoVideo.Remove(photoVideo);
+            db.SaveChanges();
+            if (photoVideo.isSlika)
+            {
+                return RedirectToAction("Slika", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Video", "Home");
+            }
         }
     }
 }
